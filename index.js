0e402974aebcf81c6c5464d79e6c46029cc66cb7
Harrison Healey <harrisonmhealey@gmail.com>
2020-03-04 16:37:21 -0500

    Move configs into an ESLint plugin

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+    configs: {
+        base: {
+            extends: [
+                require.resolve('./configs/.eslintrc.json'),
+            ],
+        },
+        react: {
+            extends: [
+                require.resolve('./configs/.eslintrc.json'),
+                require.resolve('./configs/.eslintrc-react.json'),
+            ],
+        },
+    },
+    rules: {
+    },
+};
