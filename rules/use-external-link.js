f8ca36be5ceb398a9b1c343251f19d27c89eeb6b
Nick Misasi <nick.misasi@mattermost.com>
2023-02-27 10:35:49 -0500

    Remove some unneeeded code

--- rules/use-external-link.js
+++ rules/use-external-link.js
@@ -2,7 +2,6 @@
 // See LICENSE.txt for license information.
 
 const astUtils = require('jsx-ast-utils')
-const getElementType = require('eslint-plugin-jsx-a11y/lib/util/getElementType')
 
 module.exports =  {
     meta: {
@@ -12,21 +11,15 @@ module.exports =  {
     },
 
 create: (context) => {
-    const elementType = getElementType(context);
         return {
             JSXOpeningElement: (node) => {
                 const { attributes } = node;
-                const options = context.options[0] || {};
-                const componentOptions = options.components || [];
-                const typeCheck = ['a'].concat(componentOptions);
-                const nodeType = elementType(node);
+                const typeCheck = 'a';
                 // Only check anchor elements and custom types.
-                if (typeCheck.indexOf(nodeType) === -1) {
+                if (typeCheck === nodeType) {
                     return;
                 }
-
-                const propOptions = options.specialLink || [];
-                const propsToValidate = ['target'].concat(propOptions);
+                const propsToValidate = ['target']
                 const values = propsToValidate.map((prop) => astUtils.getPropValue(astUtils.getProp(node.attributes, prop)));
                 // Checks if the target attribute is set to _blank (ie, is an external link)
                 const hasAnyTarget = values.some((value) => value != null && value === '_blank');
